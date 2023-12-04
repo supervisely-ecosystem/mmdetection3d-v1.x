62d59e36bb8a27272fa50982092b72e77e3f800e
max-unfinity <poldersen2@gmail.com>
2023-12-04 15:39:10 +0000

    rm files

--- convert_data.sh
+++
@@ -1,5 +0,0 @@
-# Download data split
-wget -c  https://raw.githubusercontent.com/traveller59/second.pytorch/master/second/data/ImageSets/test.txt --no-check-certificate --content-disposition -O ./kitti_sample/ImageSets/test.txt
-wget -c  https://raw.githubusercontent.com/traveller59/second.pytorch/master/second/data/ImageSets/train.txt --no-check-certificate --content-disposition -O ./kitti_sample/ImageSets/train.txt
-wget -c  https://raw.githubusercontent.com/traveller59/second.pytorch/master/second/data/ImageSets/val.txt --no-check-certificate --content-disposition -O ./kitti_sample/ImageSets/val.txt
-wget -c  https://raw.githubusercontent.com/traveller59/second.pytorch/master/second/data/ImageSets/trainval.txt --no-check-certificate --content-disposition -O ./kitti_sample/ImageSets/trainval.txt
