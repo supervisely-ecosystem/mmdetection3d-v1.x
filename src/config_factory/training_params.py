8a3bf6f712143a5974f1403267ace6b46394eb40
max-unfinity <poldersen2@gmail.com>
2023-12-13 18:22:35 +0000

    code improvements

--- src/config_factory/training_params.py
+++ src/config_factory/training_params.py
@@ -1,4 +1,8 @@
-from mmengine import Config
+import logging
+from mmengine.config import Config
+from mmengine.logging import print_log
+from mmdet3d.registry import RUNNERS
+from mmengine.runner import Runner
 
 
 def get_train_cfgs(max_epochs: int, val_interval: int):
@@ -30,4 +34,69 @@ def configure_training_params(cfg: Config, max_epochs: int, val_interval: int):
     # optimizer
     cfg.optim_wrapper = dict(
         type='OptimWrapper',
-        optimizer=dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001))
+        optimizer=dict(type='Adam', lr=2e-4))
+        # optimizer=dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001))
+
+
+def configure_init_weights_and_resume(cfg: Config, mmdet_checkpoint_path: str = None, supervisely_checkpoint_path: str = None, resume: bool = False):
+    # We need 4 options to support:
+    # 1. init weights from zero
+    # 2. init weights from pretrained (by mmdet3d)
+    # 3. load checkpoint trained in Supervisely (don't resume training)
+    # 4. load checkpoint trained in Supervisely and resume training
+
+    # Let's traverse the options
+    # 1. init weights from zero
+    if mmdet_checkpoint_path is None and supervisely_checkpoint_path is None:
+        cfg.resume = False
+        cfg.load_from = None
+    # 2. init weights from pretrained (by mmdet3d)
+    elif mmdet_checkpoint_path is not None and supervisely_checkpoint_path is None:
+        cfg.resume = False
+        cfg.load_from = mmdet_checkpoint_path
+    # 3. load checkpoint trained in Supervisely (don't resume training)
+    elif mmdet_checkpoint_path is None and supervisely_checkpoint_path is not None and resume is False:
+        cfg.resume = False
+        cfg.load_from = supervisely_checkpoint_path
+    # 4. load checkpoint trained in Supervisely and resume training
+    elif mmdet_checkpoint_path is None and supervisely_checkpoint_path is not None and resume is True:
+        cfg.resume = True
+        cfg.load_from = supervisely_checkpoint_path
+    else:
+        raise ValueError("Invalid combination of checkpoint paths")
+    
+
+def build_runner(cfg: Config, work_dir: str, amp: bool, auto_scale_lr: bool = False) -> Runner:
+
+    cfg.work_dir = work_dir
+
+    # enable automatic-mixed-precision training
+    if amp is True:
+        optim_wrapper = cfg.optim_wrapper.type
+        if optim_wrapper == 'AmpOptimWrapper':
+            print_log(
+                'AMP training is already enabled in your config.',
+                logger='current',
+                level=logging.WARNING)
+        else:
+            assert optim_wrapper == 'OptimWrapper', (
+                '`--amp` is only supported when the optimizer wrapper type is '
+                f'`OptimWrapper` but got {optim_wrapper}.')
+            cfg.optim_wrapper.type = 'AmpOptimWrapper'
+            cfg.optim_wrapper.loss_scale = 'dynamic'
+
+    # enable automatically scaling LR (for multi-gpu training)
+    if auto_scale_lr:
+        if 'auto_scale_lr' in cfg and \
+                'enable' in cfg.auto_scale_lr and \
+                'base_batch_size' in cfg.auto_scale_lr:
+            cfg.auto_scale_lr.enable = True
+        else:
+            raise RuntimeError('Can not find "auto_scale_lr" or '
+                               '"auto_scale_lr.enable" or '
+                               '"auto_scale_lr.base_batch_size" in your'
+                               ' configuration file.')
+
+    runner = RUNNERS.build(cfg)
+    
+    return runner
--- src/tests/test_train_origin.py
+++ src/tests/test_train_origin.py
@@ -1,92 +1,21 @@
-import logging
-
 from mmengine.config import Config
-from mmengine.logging import print_log
-from mmdet3d.registry import RUNNERS
-from mmengine.runner import Runner
+from src.config_factory.training_params import configure_init_weights_and_resume, build_runner
+from src.tests.extract_weights_url import find_weights_url
+import re
 
 
-def build_runner(cfg: Config, work_dir: str, amp: bool, auto_scale_lr: bool = False) -> Runner:
+# Model
+# cfg_model = "mmdetection3d/configs/pointpillars/pointpillars_hv_secfpn_8xb6-160e_kitti-3d-3class_custom_nus_eval.py"
+# cfg_model = "mmdetection3d/configs/point_rcnn/point-rcnn_8xb2_kitti-3d-3class_custom.py"
+cfg_model = "mmdetection3d/configs/pointpillars/pointpillars_hv_fpn_sbn-all_8xb4-2x_nus-3d_custom.py"
+model_index = "mmdetection3d/model-index.yml"
+weights_url = find_weights_url(model_index, re.sub("_custom.*\.py", ".py", cfg_model))
 
-    cfg.work_dir = work_dir
+# Make config
+cfg = Config.fromfile(cfg_model)
+configure_init_weights_and_resume(cfg, mmdet_checkpoint_path=weights_url)
 
-    # enable automatic-mixed-precision training
-    if amp is True:
-        optim_wrapper = cfg.optim_wrapper.type
-        if optim_wrapper == 'AmpOptimWrapper':
-            print_log(
-                'AMP training is already enabled in your config.',
-                logger='current',
-                level=logging.WARNING)
-        else:
-            assert optim_wrapper == 'OptimWrapper', (
-                '`--amp` is only supported when the optimizer wrapper type is '
-                f'`OptimWrapper` but got {optim_wrapper}.')
-            cfg.optim_wrapper.type = 'AmpOptimWrapper'
-            cfg.optim_wrapper.loss_scale = 'dynamic'
-
-    # enable automatically scaling LR (for multi-gpu training)
-    if auto_scale_lr:
-        if 'auto_scale_lr' in cfg and \
-                'enable' in cfg.auto_scale_lr and \
-                'base_batch_size' in cfg.auto_scale_lr:
-            cfg.auto_scale_lr.enable = True
-        else:
-            raise RuntimeError('Can not find "auto_scale_lr" or '
-                               '"auto_scale_lr.enable" or '
-                               '"auto_scale_lr.base_batch_size" in your'
-                               ' configuration file.')
-
-    runner = RUNNERS.build(cfg)
-    
-    return runner
-
-
-def configure_init_weights_and_resume(cfg: Config, mmdet_checkpoint_path: str = None, supervisely_checkpoint_path: str = None, resume: bool = False):
-    # We need 4 options to support:
-    # 1. init weights from zero
-    # 2. init weights from pretrained (by mmdet3d)
-    # 3. load checkpoint trained in Supervisely (don't resume training)
-    # 4. load checkpoint trained in Supervisely and resume training
-
-    # Let's traverse the options
-    # 1. init weights from zero
-    if mmdet_checkpoint_path is None and supervisely_checkpoint_path is None:
-        cfg.resume = False
-        cfg.load_from = None
-    # 2. init weights from pretrained (by mmdet3d)
-    elif mmdet_checkpoint_path is not None and supervisely_checkpoint_path is None:
-        cfg.resume = False
-        cfg.load_from = mmdet_checkpoint_path
-    # 3. load checkpoint trained in Supervisely (don't resume training)
-    elif mmdet_checkpoint_path is None and supervisely_checkpoint_path is not None and resume is False:
-        cfg.resume = False
-        cfg.load_from = supervisely_checkpoint_path
-    # 4. load checkpoint trained in Supervisely and resume training
-    elif mmdet_checkpoint_path is None and supervisely_checkpoint_path is not None and resume is True:
-        cfg.resume = True
-        cfg.load_from = supervisely_checkpoint_path
-    else:
-        raise ValueError("Invalid combination of checkpoint paths")
-    
-    return cfg
-
-
-if __name__ == "__main__":
-    from src.tests.extract_weights_url import find_weights_url
-
-    # Model
-    cfg_model = "mmdetection3d/configs/pointpillars/pointpillars_hv_secfpn_8xb6-160e_kitti-3d-3class_custom_nus_eval.py"
-    # cfg_model = "mmdetection3d/configs/point_rcnn/point-rcnn_8xb2_kitti-3d-3class_custom.py"
-    model_index = "mmdetection3d/model-index.yml"
-    import re
-    weights_url = find_weights_url(model_index, re.sub("_custom.*\.py", ".py", cfg_model))
-    
-    # make config
-    cfg = Config.fromfile(cfg_model)
-    cfg = configure_init_weights_and_resume(cfg, mmdet_checkpoint_path=weights_url)
-
-    runner = build_runner(cfg, "app_data/work_dir", amp=False, auto_scale_lr=False)
-    
-    runner.train()
+# Runner
+runner = build_runner(cfg, "app_data/work_dir", amp=False, auto_scale_lr=False)
+runner.train()
 
