d86e179e18c6b70e3f0e2a051d0f7a369209941c
max-unfinity <poldersen2@gmail.com>
2024-01-12 17:09:54 +0000

    zero_aux_dims in infer_all test

--- src/tests/infer_all.py
+++ src/tests/infer_all.py
@@ -5,7 +5,7 @@ import supervisely as sly
 from mmengine.config import Config
 from mmdet3d.apis import LidarDet3DInferencer
 from src.tests.extract_weights_url import find_weights_url
-from src.sly_utils import download_point_cloud, upload_point_cloud
+from src.sly_utils import download_point_cloud, upload_point_cloud, add_classes_to_project_meta
 from src.inference.pcd_inferencer import PcdDet3DInferencer
 from src.inference.functional import create_sly_annotation, up_bbox3d, filter_by_confidence
 from src.pcd_utils import convert_bin_to_pcd
@@ -19,27 +19,27 @@ warnings.filterwarnings("ignore")
 api = sly.Api()
 workspace_id = 992
 # create project with pointclouds
-project_info = api.project.get_or_create(workspace_id, "PointPillars", sly.ProjectType.POINT_CLOUDS)
+project_info = api.project.get_or_create(workspace_id, "with zero_aux", sly.ProjectType.POINT_CLOUDS)
 project_id = project_info.id
 dataset_info = api.dataset.get_or_create(project_id, "test")
 dataset_id = dataset_info.id
 # pcd_path = "app_data/lyft/LYFT/pointcloud/host-a005_lidar1_1231201454801395736.pcd"
-# pcd_path = "app_data/lyft/LYFT/pointcloud/host-a005_lidar1_1231201437602160096.pcd"
-pcd_path = "app_data/sly_project/ds0/pointcloud/000021.pcd"
+pcd_path = "app_data/lyft/LYFT/pointcloud/host-a005_lidar1_1231201437602160096.pcd"
+# pcd_path = "app_data/sly_project/ds0/pointcloud/000021.pcd"
 project_meta = sly.ProjectMeta.from_json(api.project.get_meta(project_id))
 is_bin = False
 
 with open("model_list.json", "r") as f:
     model_list = json.load(f)["detection_3d"]
 
-start = 1
-end = 2
+start = 0
+end = None
 for i, model in enumerate(model_list[start:end], start):
     name = model["name"]
-    config_info = model["pre_trained_configs"][2]
+    config_info = model["pre_trained_configs"][0]
     cfg_model = "mmdetection3d/"+config_info["config"]
     weights_url = config_info["weights"]
-    print(f"Model: {name}")
+    print(f"Model: {name} ({cfg_model})")
 
     # Make config
     cfg = Config.fromfile(cfg_model)
@@ -47,23 +47,17 @@ for i, model in enumerate(model_list[start:end], start):
     trained_dataset_name = cfg.dataset_type
     print(f"Model class names: {model_class_names}")
 
-    # add classes to project meta
-    need_update = False
-    for class_name in model_class_names:
-        if project_meta.get_obj_class(class_name) is None:
-            from supervisely.geometry.cuboid_3d import Cuboid3d
-            project_meta = project_meta.add_obj_class(sly.ObjClass(class_name, Cuboid3d))
-            print(f"Added class {class_name} to project meta.")
-            need_update = True
-    if need_update:
-        api.project.update_meta(project_id, project_meta.to_json())
-        api.project.pull_meta_ids(project_id, project_meta)
+    zero_aux_dims = cfg.dataset_type == "KittiDataset"
+    print(f"Zero aux dims: {zero_aux_dims}")
+
+    project_meta = add_classes_to_project_meta(api, project_meta, project_id, model_class_names)
 
     # Inference
     if is_bin:
         inferencer = LidarDet3DInferencer(cfg_model, weights_url, device='cuda:0')
     else:
-        inferencer = PcdDet3DInferencer(cfg_model, weights_url, device='cuda:0')
+        inferencer = PcdDet3DInferencer(cfg_model, weights_url, device='cuda:0', zero_aux_dims=zero_aux_dims)
+        inferencer.show_progress = False
 
     results_dict = inferencer(inputs=dict(points=pcd_path), no_save_vis=True)
     predictions = results_dict['predictions'][0]
--- todo2.md
+++ todo2.md
@@ -3,7 +3,6 @@
     Inference class in sdk
     протестить все модели в цикле
     протестить применялку
-    add CenterFormer
     add zero_aux_dims to Kitti models
 - train/val splits
 - visualization hooks
