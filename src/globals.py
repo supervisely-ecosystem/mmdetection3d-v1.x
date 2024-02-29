5e9f00f01384a29bfaad3e094c563b1bd5eccf53
max-unfinity <poldersen2@gmail.com>
2024-02-29 13:55:23 +0000

    fix raising project_id not found when running Serving

--- src/globals.py
+++ src/globals.py
@@ -19,10 +19,14 @@ stop_training = False
 config_name: str = None
 params: TrainParameters = None
 
-PROJECT_ID = sly.env.project_id()
+PROJECT_ID = sly.env.project_id(raise_not_found=False)  # None if running Serve App
 TEAM_ID = sly.env.team_id()
 
-PROJECT_INFO = api.project.get_info_by_id(PROJECT_ID)
+PROJECT_INFO = None
+IMAGES_COUNT = None
+if PROJECT_ID is not None:
+    PROJECT_INFO = api.project.get_info_by_id(PROJECT_ID)
+    IMAGES_COUNT = api.project.get_info_by_id(PROJECT_ID).items_count
 
 PROJECT_DIR = app_dir + "/sly_project"
 WORK_DIR = app_dir + "/work_dir"
@@ -34,11 +38,6 @@ os.makedirs(STATIC_DIR, exist_ok=True)
 NUSCENES_METRIC_KEYS = ["mAP", "NDS"]
 MAX_CLASSES_TO_SHOW_CLASSWISE_METRIC = 10
 
-# params
-# MAX_CLASSES_FOR_PERCLASS_METRICS = 10
-
-# state = State()
-IMAGES_COUNT = api.project.get_info_by_id(PROJECT_ID).items_count
 
 stop_training = False
 
