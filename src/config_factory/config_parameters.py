d8ce3a8e8942c619bed432e8d65e1ee56c20a23f
GoldenAnpu <seryios.sych@gmail.com>
2024-08-19 19:46:45 +0200

    Update cfg.model.pts_voxel_encoder for CenterPoint models

--- src/config_factory/config_parameters.py
+++ src/config_factory/config_parameters.py
@@ -302,7 +302,7 @@ def write_parameters_to_config_2(
     if cfg.model.type == "CenterPoint":
         # in_channaels
         if cfg.model.get("pts_voxel_encoder") is not None:
-            cfg.model.pts_voxel_encoder.num_features = p.in_channels
+            cfg.model.pts_voxel_encoder.in_channels = p.in_channels
 
         # tasks
         cfg.model.pts_bbox_head.tasks = [
